f0dd88ae50f0f162eebe942a839e74738780661c
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-09-03 22:52:39 +0100

    Refactor UsersController postNew method to improve readability and error handling

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,38 +7,34 @@ const userQueue = new Queue('email sending');
 
 export default class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body || {};
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
 
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
+    const user = await (await dbClient.usersCollection()).findOne({ email });
 
-    try {
-      const usersCollection = await dbClient.usersCollection();
-      const userExists = await usersCollection.findOne({ email });
-
-      if (userExists) {
-        return res.status(400).json({ error: 'Already exist' });
-      }
-
-      const hashedPassword = sha1(password);
-      const insertionResult = await usersCollection.insertOne({ email, password: hashedPassword });
-      const userId = insertionResult.insertedId.toString();
-
-      userQueue.add({ userId });
-      return res.status(201).json({ email, id: userId });
-
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
     }
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertionInfo.insertedId.toString();
+
+    userQueue.add({ userId });
+    res.status(201).json({ email, id: userId });
   }
 
   static async getMe(req, res) {
-    const { email, _id: id } = req.user;
+    const { user } = req;
 
-    return res.status(200).json({ email, id: id.toString() });
+    res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
