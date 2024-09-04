56d868cee833bab6e8665b3ccd3ef8bae6f9000c
ogeeke <ogechieke2014@gmail.com>
2024-09-04 20:14:45 +0100

    add file data

--- routes/index.js
+++ routes/index.js
@@ -71,6 +71,11 @@ function controllerRouting(app) {
   router.put('/files/:id/unpublish', (req, res) => {
     FilesController.putUnpublish(req, res);
   });
+
+  // should return the content of the file document based on the ID
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 }
 
 export default controllerRouting;
