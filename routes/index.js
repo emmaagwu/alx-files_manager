e3100d60d854bb4cd4de47bdfe9ca86440081e35
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-09-04 10:57:37 +0100

    Refactor AppController and routes***

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,18 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import UsersController from '../controllers/UsersController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
 
-/**
- * Injects routes with their handlers to the given Express application.
- * @param {Express} api
- */
-const injectRoutes = (api) => {
-  api.get('/status', AppController.getStatus);
-  api.get('/stats', AppController.getStats);
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-  api.get('/connect', basicAuthenticate, AuthController.getConnect);
-  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+  // App Controller
 
-  api.post('/users', UsersController.postNew);
-  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-
-  api.all('*', (req, res, next) => {
-    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
+  // should return if Redis is alive and if the DB is alive
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
   });
-  api.use(errorResponse);
-};
 
-export default injectRoutes;
+  // should return the number of users and files in DB
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
